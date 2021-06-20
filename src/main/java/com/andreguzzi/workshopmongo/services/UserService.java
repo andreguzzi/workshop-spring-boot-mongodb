6669c689766a5a964599219a74774d410dd0fad6
Andre Guzzi <andre_guzzi@hotmail.com>
2021-06-20 20:36:23 -0300

    Obtendo um usuário por id

--- src/main/java/com/andreguzzi/workshopmongo/services/UserService.java
+++ src/main/java/com/andreguzzi/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andreguzzi.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andreguzzi.workshopmongo.domain.User;
 import com.andreguzzi.workshopmongo.repository.UserRepository;
+import com.andreguzzi.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,9 @@ public class UserService {
 		
 	}
 
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
 }
