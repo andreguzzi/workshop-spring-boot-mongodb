3f62dc729e2d9ae6967ea3f280d802c1f3439743
Andre Guzzi <andre_guzzi@hotmail.com>
2021-06-20 17:53:25 -0300

    projeto criado

---
+++ src/test/java/com/andreguzzi/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreguzzi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
