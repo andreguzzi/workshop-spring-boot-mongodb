5f70753c9cc39ea5be69750e4a71ef3510032478
Andre Guzzi <andre_guzzi@hotmail.com>
2021-06-21 09:52:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/andreguzzi/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.andreguzzi.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andreguzzi.workshopmongo.domain.Post;
+import com.andreguzzi.workshopmongo.repository.PostRepository;
+import com.andreguzzi.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
